975f1f07f066e69c080c28637b92ba16a021f45b
Dan <42104020+DanFrmSpace@users.noreply.github.com>
2020-02-12 22:21:33 -0500

    Day One upload

---
+++ Day One/MyPlayground.playground/Contents.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+var str = "Hello, playground"
+str = "New world order!" // Changed the intial variable
+
+var age = 38
+var population = 9_000_000
+
+var mLineStr = """
+This is a muiltiline
+string that is used to make
+the code look better
+"""
+
+var mLineStr2 = """
+This is a multiline \
+string that is used to make \
+your code look better but stay on one line
+"""
+
+var pi = 3.141 // This is a double used to store decimal values
+var choice = false // This is a bool. (true/ false)
+
+var name = "Dan"
+var nAge = 23
+
+var sent = "My name is \(name), and I am \(nAge) years old" // String Interpolation
+
+let nName = "Dantavious" // let is used for a constant data type. It doesn't change.
+
+// type annotations
+let album: String = "Repuatation"
+let year: Int = 1996
+let height: Double = 1.76
+let Dan: Bool = true
+
+// Day One of learning Swift. Variable, Simple Data Types, and String Interpolatoion
